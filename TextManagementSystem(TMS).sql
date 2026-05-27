c5926767ab1101eb1d24ab2ff18475a13fd28d4d
ANUSHALAV <bhattanushalav@gmail.com>
2026-05-27 20:18:27 +0530

    add data in user table

--- TextManagementSystem(TMS).sql
+++ TextManagementSystem(TMS).sql
@@ -3,7 +3,7 @@ CREATE DATABASE TMS;
 USE TMS;
 
 CREATE TABLE UserType(
-UserTypeID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+UserTypeId INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 UserType VARCHAR(255) NOT NULL,
 Status INT NOT NULL DEFAULT 1
 );
@@ -19,7 +19,7 @@ SELECT * FROM UserType;
 
 CREATE TABLE User(
 UserId INT PRIMARY KEY AUTO_INCREMENT,
-UserTypeId INT NOT NULL,
+UserTypeId INT, FOREIGN KEY(UserTypeId) REFERENCES UserType(UserTypeId),
 FirstName VARCHAR(255) NOT NULL,
 LastName VARCHAR(255) NOT NULL,
 Address VARCHAR(255) DEFAULT Null,
@@ -29,7 +29,14 @@ Email VARCHAR(255) DEFAULT Null,
 LoginId VARCHAR(255) NOT NULL,
 Password VARCHAR(255) NOT NULL,
 Status INt NOT NULL DEFAULT 1
-);
+) AUTO_INCREMENT = 1001;
+
+
+INSERT INTO User (UserTypeId,FirstName,LastName,Address,Gender,Phone,Email,LoginId,Password) VALUES
+(1,"Anushalav","Bhatt","Dhalwala","Male","8909506360","superAdmin@gmail.com","superAdmin","superAdmin"),
+(2,"Aman","Singh","Dhalwala","Male","9876543234","admin@gmail.com","admin","admin");
+
 
 SELECT * FROM User;
+
 
