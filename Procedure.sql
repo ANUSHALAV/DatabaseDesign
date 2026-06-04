ff907110d31f9b997d6a09067ed03b45b202228f
ANUSHALAV <bhattanushalav@gmail.com>
2026-06-04 21:09:49 +0530

    add procedure for getdistrict

--- Procedure.sql
+++ Procedure.sql
@@ -8,4 +8,29 @@ DELIMITER ;
 
 CALL GetStateByCountryId(1);
 
+
+DELIMITER $$
+CREATE PROCEDURE GetDistrictByCountryIdAndStateId
+(
+    IN c_CountryId INT,
+    IN s_StateId INT
+)
+BEGIN
+    SELECT
+      d.DistrictId , d.DistrictName,s.StateId,s.StateName,s.CountryId,c.CountryName,s.Status
+    FROM District AS d
+    INNER JOIN Country AS c
+        ON c.CountryId = d.CountryId
+    INNER JOIN State AS s
+        ON s.StateId = d.StateId
+    WHERE d.CountryId = c_CountryId
+      AND d.StateId = s_StateId;
+
+END $$
+DELIMITER ;
+
+CALL GetDistrictByCountryIdAndStateId(1,1);
+
+
+
  
