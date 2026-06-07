341ac0632805fb67e7f6325ff98b07d0a70c79dd
ANUSHALAV <bhattanushalav@gmail.com>
2026-06-07 21:03:05 +0530

    set query arrangement

--- TextManagementSystem(TMS).sql
+++ TextManagementSystem(TMS).sql
@@ -16,7 +16,20 @@ INSERT INTO UserType(UserType) VALUES
 ("Driver"),
 ("Passenger");
 
-SELECT * FROM UserType;
+
+CREATE TABLE Route(
+ RouteId INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+ RouteName VARCHAR(255),
+ PricePerPerson VARCHAR(255),
+ Status INT DEFAULT 1
+);
+
+INSERT INTO Route (RouteName,PricePerPerson) VALUES
+("Rishikesh To Dehraedun" , "100"),
+("Prshikesh To Devprayad","150"),
+("Rishikesh To Haridwar","60"),
+("Rishikesh To Delhi","550"),
+("Rishkesh To Mohali","500");
 
 
 CREATE TABLE IF NOT EXISTS Country (
@@ -30,9 +43,6 @@ INSERT INTO Country (CountryName) VALUES
 ('United States'),
 ('Canada');
 
-SELECT * FROM Country;
-
-DELETE FROM Country WHERE CountryId = 7;
 
 CREATE TABLE IF NOT EXISTS State (
 StateId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -48,8 +58,6 @@ INSERT INTO State (CountryId, StateName) VALUES
 (2, 'California'),
 (2, 'Texas'),
 (3, 'Ontario');
-
-SELECT * FROM State;
 
 CREATE TABLE IF NOT EXISTS District(
 DistrictId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -80,10 +88,6 @@ INSERT INTO District (CountryId, StateId, DistrictName) VALUES
 (3, 6, 'Ottawa');
 
 
-SELECT * FROM District;
-
-
-
 
 CREATE TABLE User(
 UserId INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
@@ -109,41 +113,8 @@ Status INt NOT NULL DEFAULT 1
 INSERT INTO User (UserTypeId,FirstName,LastName,Address,State,District,PinCode,Gender,Phone,Email,LoginId,Password) VALUES
 (1,"Anushalav","Bhatt","Dhalwala",3,3,456754,"Male","8909506360","superAdmin@gmail.com","superAdmin","superAdmin"),
 (2,"Aman","Singh","Dhalwala",1,2,345634,"Male","9876543234","admin@gmail.com","admin","admin");
-
-
-SELECT * FROM User;
-
-CREATE TABLE Route(
- RouteId INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
- RouteName VARCHAR(255),
- PricePerPerson VARCHAR(255),
- Status INT DEFAULT 1
-);
-
-INSERT INTO Route (RouteName,PricePerPerson) VALUES
-("Rishikesh To Dehraedun" , "100"),
-("Prshikesh To Devprayad","150"),
-("Rishikesh To Haridwar","60"),
-("Rishikesh To Delhi","550"),
-("Rishkesh To Mohali","500");
-
-SELECT * FROM Route;
-
--- For driver we can add some more columns..
-ALTER TABLE User ADD(
-AadharNumber VARCHAR(255),
-LisenseNumber VARCHAR(255),
-PenCardNumber VARCHAR(255)
- );
  
--- For any types of user add some columns.. 
- ALTER TABLE User ADD(
- State INT NOT NULL,
- District INT NOT NULL,
- PinCode INT NOT NULL
- );
- 
- CREATE TABLE Passenger(
+CREATE TABLE Passenger(
  PassengerId INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
  FirstName VARCHAR(255),
  LastName VARCHAR(255),
@@ -154,21 +125,32 @@ PenCardNumber VARCHAR(255)
  IsAlone bool NOT NULL DEFAULT TRUE,
  TotalPassengerWhoTraval Int NOT NULL,
  Status INT DEFAULT 1
- );
+);
  
- INSERT INTO User (UserTypeId,FirstName,LastName,Address,Gender,State,District,PinCode,AadharNumber,LisenseNumber,PenCardNumber,Phone,Email,LoginId,Password) VALUES
+INSERT INTO User (UserTypeId,FirstName,LastName,Address,Gender,State,District,PinCode,AadharNumber,LisenseNumber,PenCardNumber,Phone,Email,LoginId,Password) VALUES
 (3,"Anushalav","Bhatt","Dhalwala","Male",1,1,789098,"656565432123","UK6545TY67876","FLPIY6543","8909506360","anushalav@gmail.com","anushalav","anushalav"),
 (3,"Aman","Singh","Dhalwala","Male",1,1,789048,"656535432123","UK6545TY611111","FLPIY1111","9876543234","aman@gmail.com","aman","aman");
+
  
- ALTER TABLE Passenger DROP COLUMN Address;
- 
- ALTER TABLE Passenger MODIFY COLUMN TotalPassengerWhoTraval INT NOT NULL DEFAULT 1;
- 
- SELECT * FROM Passenger;
- 
- INSERT INTO Passenger (FirstName,LastName,Phone,RouteId,DriverId,IsAlone,TotalPassengerWhoTraval) VALUES
+INSERT INTO Passenger (FirstName,LastName,Phone,RouteId,DriverId,IsAlone,TotalPassengerWhoTraval) VALUES
  ("Anushaalav","Bhatt","8909506360",1,101,true,1),
  ("Aman","Singh","9876543456",2,102,false,3);
+
+
+SELECT * FROM UserType;
+
+SELECT * FROM Route;
+
+SELECT * FROM Country;
+
+SELECT * FROM State;
+
+SELECT * FROM District;
+
+SELECT * FROM User;
+
+SELECT * FROM Passenger;
+
 
 
 
